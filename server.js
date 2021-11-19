47d1374e8285d26ea70727a67fbb6ee7fabba1bf
runner <runner@Mac-1637298919388.local>
2021-11-19 05:40:19 +0000

    replace bare import specificiers with urls

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import ultra, { app, router } from "ultra";
-import anybar from "anybar";
-import postcss from "postcss";
-import postcssNesting from "postcss-nesting";
-import { Snelm } from "snelm";
+import ultra, { app, router } from "https://raw.githubusercontent.com/nnmrts/ultra/main/mod.ts"
+import anybar from "https://x.nest.land/anybar@0.1.3/module.js"
+import postcss from "https://deno.land/x/postcss/mod.js"
+import postcssNesting from "https://cdn.jsdelivr.net/npm/postcss-nesting@10/mod.js"
+import { Snelm } from "https://deno.land/x/snelm/mod.ts"
 
 const {
 	readTextFile
